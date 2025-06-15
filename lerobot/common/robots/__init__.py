4303568bc534000698449f95eb7726c23d3f896b
DongWook Lee <sh95119@gmail.com>
2025-06-15 10:03:35 +0900

    PATCH

--- lerobot/common/robots/__init__.py
+++ lerobot/common/robots/__init__.py
@@ -1,3 +1,6 @@
 from .config import RobotConfig
 from .robot import Robot
 from .utils import make_robot_from_config
+
+# Import all robot modules
+from . import koch_follower, so100_follower, so101_follower, bimanual_so100_follower, bimanual_so101_follower
--- lerobot/common/teleoperators/__init__.py
+++ lerobot/common/teleoperators/__init__.py
@@ -1,3 +1,6 @@
 from .config import TeleoperatorConfig
 from .teleoperator import Teleoperator
 from .utils import make_teleoperator_from_config
+
+# Import all teleoperator modules
+from . import koch_leader, so100_leader, so101_leader, bimanual_so100_leader, bimanual_so101_leader
