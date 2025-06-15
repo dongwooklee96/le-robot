ab579ebca6d6aeabec1ed3b938d3f9be099d8ed9
DongWook Lee <sh95119@gmail.com>
2025-06-15 10:05:59 +0900

    PATCH3

--- lerobot/common/robots/__init__.py
+++ lerobot/common/robots/__init__.py
@@ -1,6 +1,3 @@
 from .config import RobotConfig
 from .robot import Robot
 from .utils import make_robot_from_config
-
-# Import all robot modules
-from . import koch_follower, so100_follower, so101_follower, bimanual_so100_follower, bimanual_so101_follower
